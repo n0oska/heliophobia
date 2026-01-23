eb7a4f34b9b2b3ee4fb60111c59330def56d8d98
HatiSah <gabrielviala22@gmail.com>
2026-01-23 18:00:23 +0100

    ziz

--- AGENTS.md
+++ AGENTS.md
@@ -1,6 +1,12 @@
 <!-- UNITY CODE ASSIST INSTRUCTIONS START -->
 - Project name: heliophobia
 - Unity version: Unity 6000.2.7f2
+- Active scene:
+  - Name: LV1
+  - Tags:
+    - Untagged, Respawn, Finish, EditorOnly, MainCamera, Player, GameController, Coin, TriggerCam, Spawner, TestCoin, TestTrigger
+  - Layers:
+    - Default, TransparentFX, Ignore Raycast, Obstacle, Water, UI, Enemy, Destroyable, Player, Camera
 - Active game object:
   - Name: P_Dialogue
   - Tag: Untagged
--- Assets/Scripts/UI&Menu/S_DialogueUI.cs
+++ Assets/Scripts/UI&Menu/S_DialogueUI.cs
@@ -2,30 +2,64 @@ using UnityEngine;
 using UnityEditor.UI;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class S_DialogueUI : MonoBehaviour
 {
     [SerializeField] private Canvas m_canvas;
     [SerializeField] private Image m_image;
-    [SerializeField] private TextMeshProUGUI m_text;
+    [SerializeField] private TextMeshProUGUI m_textToShow;
+    [SerializeField] private List<TextMeshProUGUI> m_textList;
+    
+
     //[SerializeField] private TextMeshProUGUI nextTextToshow;
 
     private bool hasDialogueEnded = false;
+    private bool hasSkipped = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        m_canvas.enabled = true;
-        m_image.enabled = true;
-        m_text.enabled = true;
-        hasDialogueEnded = true;
+        StartCoroutine(C_EcrisTaMere());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (hasDialogueEnded && Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
-            m_canvas.enabled = false;
+            hasSkipped = true;
+        }
+
+        if (hasDialogueEnded)
+        {
+            StartCoroutine(C_EcrisTaMere());
+        }
+    }
+
+    private IEnumerator C_EcrisTaMere()
+    {
+        hasSkipped = false;
+        for (int i = 0; i < m_textList.Count;)
+        {
+            if (i == m_textList.Count)
+            {
+                m_textToShow = m_textList.ElementAt(m_textList.Count);
+            }
+
+            // if (hasDialogueEnded)
+            // {
+                
+            // }
+
+            if (hasSkipped)
+            {
+                hasDialogueEnded = true;
+                i++;
+                yield return new WaitForEndOfFrame();
+            }
+            
         }
     }
 }
