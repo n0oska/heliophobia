482fc24483972579db31095af792a57fa6ef99d1
HatiSah <gabrielviala22@gmail.com>
2026-01-26 17:58:12 +0100

    Main menu fonctionnel en entier
    20 min
    
    Tout le main menu de A à Z en gros ^^

--- AGENTS.md
+++ AGENTS.md
@@ -2,13 +2,13 @@
 - Project name: heliophobia
 - Unity version: Unity 6000.2.7f2
 - Active scene:
-  - Name: LV1
+  - Name: SC_Menu
   - Tags:
     - Untagged, Respawn, Finish, EditorOnly, MainCamera, Player, GameController, Coin, TriggerCam, Spawner, TestCoin, TestTrigger
   - Layers:
     - Default, TransparentFX, Ignore Raycast, Obstacle, Water, UI, Enemy, Destroyable, Player, Camera
 - Active game object:
-  - Name: P_Dialogue
+  - Name: BTN_SoundBack
   - Tag: Untagged
   - Layer: UI
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/Scripts/UI&Menu/S_MenuManager.cs
+++ Assets/Scripts/UI&Menu/S_MenuManager.cs
@@ -4,17 +4,63 @@ using UnityEngine.UI;
 
 public class S_MenuManager : MonoBehaviour
 {
+    [Header("Main menu")]
     [SerializeField] private Button m_startButton;
+    [SerializeField] private Canvas m_canvasMain;
+    private bool isInOptions = false;
+
+    [Header("Options menu")]
+    [SerializeField] private Canvas m_canvasOptions;
+    [SerializeField] private Button m_buttonSound;
+    [SerializeField] private GameObject m_panelSound;
+    [SerializeField] private GameObject m_panelOptions;
+    [SerializeField] private Button m_backButtonPanelOptions;
+    [SerializeField] private Slider m_sliderSound;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         m_startButton.Select();
+        m_canvasOptions.enabled = false;
+        m_panelOptions.SetActive(false);
+        m_panelSound.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Options()
     {
-        
+        m_canvasOptions.enabled = true;
+        m_canvasMain.enabled = false;
+        m_buttonSound.Select();
+    }
+
+    public void OnBackClick()
+    {
+        m_canvasMain.enabled = true;
+        m_canvasOptions.enabled = false;
+        m_startButton.Select();
+    }
+
+    public void OnSoundClick()
+    {
+        m_panelSound.SetActive(true);
+        m_sliderSound.Select();
+    }
+
+    public void OnSoundBackClick()
+    {
+        m_panelSound.SetActive(false);
+        m_buttonSound.Select();
+    }
+
+    public void OnControlsClick()
+    {
+        m_panelOptions.SetActive(true);
+        m_backButtonPanelOptions.Select();
+    }
+
+    public void OnControlsBackClick()
+    {
+        m_panelOptions.SetActive(false);
+        m_buttonSound.Select();
     }
 
     public void OnStartClick()
