54e4db63d70e56322593e5a8f16a7a70734edf45
HatiSah <gabrielviala22@gmail.com>
2026-02-02 14:51:53 +0100

    Light enemy fix
    20 min de taff
    Maintenant y'a 4 chances sur 10 qu'ils droppent un coin

--- Assets/Scripts/Ennemis/S_LightEnemlyController.cs
+++ Assets/Scripts/Ennemis/S_LightEnemlyController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class S_LightEnemyController : MonoBehaviour
@@ -5,6 +6,9 @@ public class S_LightEnemyController : MonoBehaviour
     [Header("References")]
     [SerializeField] private Light m_light; 
     [SerializeField] private float m_effectRadius = 5f;
+    [SerializeField] private GameObject m_coins;
+
+    private bool isLight = false;
 
     private void OnDrawGizmosSelected()
     {
@@ -14,8 +18,7 @@ public class S_LightEnemyController : MonoBehaviour
     }
 
     private void Update()
-    {
-       
+    {       
         Collider[] players = Physics.OverlapSphere(transform.position, m_effectRadius);
         foreach (var col in players)
         {
@@ -25,5 +28,32 @@ public class S_LightEnemyController : MonoBehaviour
                 player.ForceExitShadow(); 
             }
         }
+
+        S_EnemyController scriptennemy = this.gameObject.GetComponent<S_EnemyController>();
+
+        if (scriptennemy != null)
+        {
+            isLight = true;
+        }
+
+        if (isLight && scriptennemy.m_health.isDead())
+        {
+            StartCoroutine(C_Drop());
+        }
+
+    }
+
+    private IEnumerator C_Drop()
+    {
+        Debug.Log("in coroutine drop");
+        int randomInt = Random.Range(1, 10);
+        Debug.Log(randomInt);
+
+        if (randomInt >= 6)
+        {
+            m_coins.transform.position = this.transform.position;
+            Instantiate(m_coins);
+            yield return new WaitForFixedUpdate();
+        }        
     }
 }
