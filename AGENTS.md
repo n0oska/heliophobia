11fa6330d5c6a80fff86b8450df0d5dcf02f7c95
Yacine-Goumidi <yacine.zeref@gmail.com>
2026-02-02 15:44:50 +0100

    Game Over réparé

--- AGENTS.md
+++ AGENTS.md
@@ -2,13 +2,13 @@
 - Project name: heliophobia
 - Unity version: Unity 6000.2.7f2
 - Active scene:
-  - Name: LV2
+  - Name: LV1
   - Tags:
     - Untagged, Respawn, Finish, EditorOnly, MainCamera, Player, GameController, Coin, TriggerCam, Spawner, TestCoin, TestTrigger, EndTrigger
   - Layers:
     - Default, TransparentFX, Ignore Raycast, Obstacle, Water, UI, Enemy, Destroyable, Player, Camera
 - Active game object:
-  - Name: EA_Tr_Hills_Grass_02a_LOD0 (3)
+  - Name: P_DeathScreen
   - Tag: Untagged
-  - Layer: Default
+  - Layer: UI
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/Scripts/S_DeathSCreen.cs
+++ Assets/Scripts/S_DeathSCreen.cs
@@ -4,11 +4,12 @@ using UnityEngine.SceneManagement;
 
 public class S_DeathSCreen : MonoBehaviour
 {
-    [SerializeField] Canvas m_deathScreen;
-    [SerializeField] Button m_restartButton;
-    [SerializeField] GameObject m_player;
+    [SerializeField] private Canvas m_deathScreen;
+    [SerializeField] private Button m_restartButton;
+    [SerializeField] private GameObject m_player;
 
     private S_CharaController m_charaCon;
+    private bool m_isGameOver = false;
 
     void Start()
     {
@@ -23,18 +24,40 @@ public class S_DeathSCreen : MonoBehaviour
 
     private void CheckDeath()
     {
-        if (m_charaCon.m_playerHealth.isDead())
+        if (m_charaCon.m_playerHealth.isDead() && !m_isGameOver)
         {
+            m_isGameOver = true;
+
             Debug.Log("isDead");
+
+            // Ici je check l'affichage
             m_deathScreen.enabled = true;
+
+            // je désactive du joueur (plus d'inputs détecté)
+            m_charaCon.enabled = false;
+
+            // on force l'affochage du curseur
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
+            // Le bouton est automatiquement sélectionné
             m_restartButton.Select();
-            //Time.timeScale = 0;
+
+            // Le jeu se met en pause
+            Time.timeScale = 0f;
         }
     }
 
     public void OnRestartClick()
     {
-        Time.timeScale = 1;
+        // Reprise du temps
+        Time.timeScale = 1f;
+
+        // Re-lock du curseur
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        // Reload de la scène
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
