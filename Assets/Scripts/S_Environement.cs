4cc8c4b34706dcc54da643d01df6f3127e6b59fe
HatiSah <gabrielviala22@gmail.com>
2026-02-17 16:33:36 +0100

    sah

--- Assets/Scripts/S_Environement.cs
+++ Assets/Scripts/S_Environement.cs
@@ -5,6 +5,10 @@ public class S_Environement : MonoBehaviour
 {
     public HealthManager m_health = new HealthManager();
     [SerializeField] GameObject m_coin;
+    [SerializeField] private ParticleSystem m_ennemyHitPs;
+    private ParticleSystem m_ennemyHitPsInstance;
+
+    private bool hasPsSpawned;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,7 +23,25 @@ public class S_Environement : MonoBehaviour
             StartCoroutine(DropRandom());
             Destroy(this.gameObject);
         }
-            
+
+        if (m_health.isTakingDamage && !hasPsSpawned)
+        {
+            hasPsSpawned = true;
+            StartCoroutine(C_ParticlesInstance());            
+        }
+
+        m_health.EndOfFrame();
+    }
+
+    private IEnumerator C_ParticlesInstance()
+    {        
+        m_ennemyHitPsInstance = Instantiate(m_ennemyHitPs, transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(0.2f);
+        m_ennemyHitPsInstance.Clear();
+        hasPsSpawned = false;
+
+       //m_anim.SetTrigger("TakeDamage");
+
     }
 
     private IEnumerator DropRandom()
