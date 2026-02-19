da9d78b6bb56f1319ac6e0406a1ad40a1e9d0dcd
Yacine-Goumidi <yacine.zeref@gmail.com>
2026-02-19 16:44:44 +0100

    Penviro1 ok !

--- AGENTS.md
+++ AGENTS.md
@@ -2,13 +2,13 @@
 - Project name: heliophobia
 - Unity version: Unity 6000.3.8f1
 - Active scene:
-  - Name: P_UiHealth
+  - Name: P_Enviro 1
   - Tags:
     - Untagged, Respawn, Finish, EditorOnly, MainCamera, Player, GameController, Coin, TriggerCam, Spawner, TestCoin, TestTrigger, EndTrigger
   - Layers:
     - Default, TransparentFX, Ignore Raycast, Obstacle, Water, UI, Enemy, Destroyable, Player, Camera
 - Active game object:
-  - Name: TXT_Score (1)
+  - Name: P_Enviro 1
   - Tag: Untagged
-  - Layer: UI
+  - Layer: Destroyable
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/Scripts/S_Environement.cs
+++ Assets/Scripts/S_Environement.cs
@@ -6,6 +6,9 @@ public class S_Environement : MonoBehaviour
     public HealthManager m_health = new HealthManager();
     [SerializeField] GameObject m_coin;
     [SerializeField] private ParticleSystem m_ennemyHitPs;
+    [SerializeField] private int m_lowProba;
+    [SerializeField] private int m_highProba;
+    [SerializeField] private int m_targetProba;
     private ParticleSystem m_ennemyHitPsInstance;
 
     private bool hasPsSpawned;
@@ -27,28 +30,28 @@ public class S_Environement : MonoBehaviour
         if (m_health.isTakingDamage && !hasPsSpawned)
         {
             hasPsSpawned = true;
-            StartCoroutine(C_ParticlesInstance());            
+            StartCoroutine(C_ParticlesInstance());
         }
 
         m_health.EndOfFrame();
     }
 
     private IEnumerator C_ParticlesInstance()
-    {        
+    {
         m_ennemyHitPsInstance = Instantiate(m_ennemyHitPs, transform.position, Quaternion.identity);
         yield return new WaitForSeconds(0.2f);
         m_ennemyHitPsInstance.Clear();
         hasPsSpawned = false;
 
-       //m_anim.SetTrigger("TakeDamage");
+        //m_anim.SetTrigger("TakeDamage");
 
     }
 
     private IEnumerator DropRandom()
     {
-        int randomInt = Random.Range(1, 10);
+        int randomInt = Random.Range(m_lowProba, m_highProba);
 
-        if (randomInt >=6 )
+        if (randomInt >= m_targetProba)
         {
             Debug.Log(randomInt);
             m_coin.transform.position = this.transform.position;
@@ -57,3 +60,4 @@ public class S_Environement : MonoBehaviour
         }
     }
 }
+//Dans l'inspector sur l'enviro de base : low proba =1 high proba = 10 target proba =6
